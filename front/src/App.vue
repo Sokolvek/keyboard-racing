0d7bafe7301e37447ee0632361951ba5d83d4efa
Sokolvek <vadim200608@yandex.ru>
2024-05-14 01:59:57 +0300

    trying to use websockets

--- front/src/App.vue
+++ front/src/App.vue
@@ -9,36 +9,50 @@
     </div>
     <input v-model="inputVal" @keydown="handleKey" type="text">
   </div>
+  <p>Errors: {{ sentenceErrors }}</p>
 </template>
 
 <script setup>
 
 import { onMounted, ref } from 'vue';
-
+import SockJS from 'sockjs-client';
 const sentence = ref(null)
 const sen = ref("мама мыла раму")
 const inputVal = ref("")
-const wordIndex = ref(0)
-let letterIndex = 0
 
+const wordIndex = ref(0)
+const sentenceErrors = ref(0)
+let letterIndex = 0
+let notErrorKeys = new Set(["Alt", "Shift", "Control", "CapsLock", "Tab"])
 
 function handleKey(e){
+  console.log(e)
   if(e.key == " " && inputVal.value == sen.value.split(" ")[wordIndex.value]){
     wordIndex.value += 1
     letterIndex = 0
-    inputVal.value = ""
     console.log("spacebar pressed")
+    setTimeout(() => inputVal.value = "",0)
+    inputVal.value = ""
     return
   }
 
   if(e.key == sen.value.split(" ")[wordIndex.value][letterIndex]){
     console.log("key matched")
     letterIndex++
+  }else if(!notErrorKeys.has(e.key) && e.key != sen.value.split(" ")[wordIndex.value][letterIndex]){
+    sentenceErrors.value += 1
   }
 }
 
 onMounted(() =>{
-  console.log(sentence.value)
+  try{
+    const sock = new SockJS("ws://localhost:8080/hello")
+
+  }catch(e){
+    console.log("suka blyad")
+  }
+  //sock.onopen = () => console.log("connected")
+  // socket.send("hello")
 })
 
 </script>
